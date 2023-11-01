1cdb3c773bc0f436720d44ca0d5efa9c6dcd71b9
Gina Marieges <gina.marieges@gmail.com>
2023-11-01 21:31:26 +0100

    Add ContainerStyled in app

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import App from "./components/App/App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import GlobalStyle from "./styles/GlobalStyle/GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>
