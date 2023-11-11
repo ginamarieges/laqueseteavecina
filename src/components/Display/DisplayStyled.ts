a571cfc5987181dfc86ef5d922ad4396203da929
Gina Marieges <gina.marieges@gmail.com>
2023-11-11 17:07:11 +0100

    Add styles in Display

---
+++ src/components/Display/DisplayStyled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const DisplayStyled = styled.section`
+  .text {
+    font-size: 1.3rem;
+    font-style: italic;
+    width: 500px;
+  }
+`;
+
+export default DisplayStyled;
