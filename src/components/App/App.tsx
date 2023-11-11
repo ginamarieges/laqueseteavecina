99c0d8c8636a5ee28961e579a1ef3d9ccfc0d7c4
Gina Marieges <gina.marieges@gmail.com>
2023-11-11 17:07:29 +0100

    Add Display in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import Countdown from "../Countdown/Countdown";
+import Display from "../Display/Display";
 import ContainerStyled from "../shared/ContainerStyled";
 
 const App = (): JSX.Element => {
   return (
     <ContainerStyled>
       <Countdown />
+      <Display />
     </ContainerStyled>
   );
 };
