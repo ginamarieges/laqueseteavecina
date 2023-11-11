5b1fe1299c086bef73e5b12c86715cf0820b4eaf
Gina Marieges <gina.marieges@gmail.com>
2023-11-11 20:50:28 +0100

    Modify App test

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,22 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import { Provider } from "react-redux";
-import { store } from "../../store";
 
 describe("Given an App component", () => {
   describe("When rendered", () => {
-    test("Then it should show the text 'Hello world!", () => {
-      const expectedText = /hello world!/i;
+    test("Then it should show the heading 'LA QUE SE TE AVECINA", () => {
+      const expectedText = /la que se te avecina/i;
 
-      render(
-        <Provider store={store}>
-          <App />
-        </Provider>
-      );
+      render(<App />);
 
       const text = screen.getByText(expectedText);
 
       expect(text).toBeInTheDocument();
+    });
+    test("Then it should show the texts, 'days', 'hours', 'minutes' and 'seconds'", () => {
+      const expectedDaysText = "days";
+      const expectedHoursText = "hours";
+      const expectedMinutesText = "minutes";
+      const expectedSecondsText = "seconds";
+
+      render(<App />);
+
+      const daysText = screen.getByText(expectedDaysText);
+      const hoursText = screen.getByText(expectedHoursText);
+      const minutesText = screen.getByText(expectedMinutesText);
+      const secondsText = screen.getByText(expectedSecondsText);
+
+      expect(daysText).toBeInTheDocument();
+      expect(hoursText).toBeInTheDocument();
+      expect(minutesText).toBeInTheDocument();
+      expect(secondsText).toBeInTheDocument();
     });
   });
 });
